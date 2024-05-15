e1eb03c8edac9e972701a198ce7ae3ede5323696
muratcan-yuksel <muratcanyukselpro@gmail.com>
2024-05-15 23:05:42 +0700

    fallback

--- src/components/BuyTokens.jsx
+++ src/components/BuyTokens.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+import { useWriteContract } from "wagmi";
 const BuyTokens = () => {
+  const [amount, setAmount] = useState(0);
+  const { writeContract, failureReason } = useWriteContract();
+  console.log(amount);
+
+  const callDepositLPT = async () => {
+    try {
+      writeContract({
+        abi: "abi",
+        address: "tokensale address",
+        functionName: "buyWhitesaleTokens",
+        args: [parseUnits(LPTAmount, 18)],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div style={{ maxWidth: "300px" }}>
       <h2>Buy Tokens</h2>
@@ -9,7 +28,12 @@ const BuyTokens = () => {
           justifyContent: "center",
         }}
       >
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+        />
         <button>Buy</button>
       </div>
     </div>
