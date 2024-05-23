37516c60862e16b79c8105607c018f4cd06fb572
muratcan-yuksel <muratcanyukselpro@gmail.com>
2024-05-24 00:39:37 +0700

    add only owner protection

--- src/routes/Owner.jsx
+++ src/routes/Owner.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
-import { useWriteContract, useDisconnect, useReadContract } from "wagmi";
+import {
+  useWriteContract,
+  useDisconnect,
+  useReadContract,
+  useAccount,
+} from "wagmi";
 import { parseEther, parseUnits } from "viem";
 import { awtokenAbi, awtokenAddress } from "../ABI/awtokenAbi.js";
 import { tokensaleAddress, tokensaleAbi } from "../ABI/tokensaleAbi.js";
+import { Navigate } from "react-router-dom";
 
 const Owner = () => {
   const { writeContract, status } = useWriteContract();
+  const { address, isConnected } = useAccount();
+
+  if (address !== "0x1AF34385343fdf673aedB90A26ee64Bb01e1667D") {
+    return <Navigate to="/" replace />;
+  }
 
   const toggleWhitelistSaleActive = async () => {
     try {
@@ -75,132 +86,20 @@ const Owner = () => {
   // });
 
   return (
-    <div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "start",
-          justifyContent: "center",
-        }}
-      >
-        <button
-          style={{
-            minWidth: "200px",
-            maxWidth: "200px",
-            textAlign: "start",
-            margin: "5px 5px",
-            padding: "10px",
-            borderRadius: "5px",
-            border: "1px solid #ccc",
-            marginBottom: "10px",
-            backgroundColor: "#90caf9",
-            color: "black",
-            cursor: "pointer",
-            fontWeight: "bold",
-            fontSize: "15px",
-          }}
-          onClick={() => toggleWhitelistSaleActive()}
-        >
-          toggleWhitelistSaleActive
-        </button>
-        <button
-          style={{
-            minWidth: "200px",
-            maxWidth: "200px",
-            textAlign: "start",
-            margin: "5px 5px",
-            padding: "10px",
-            borderRadius: "5px",
-            border: "1px solid #ccc",
-            marginBottom: "10px",
-            backgroundColor: "#90caf9",
-            color: "black",
-            cursor: "pointer",
-            fontWeight: "bold",
-            fontSize: "15px",
-          }}
-          onClick={() => toggleSaleActive()}
-        >
-          toggleSaleActive
-        </button>
-        <button
-          style={{
-            minWidth: "200px",
-            maxWidth: "200px",
-            textAlign: "start",
-            margin: "5px 5px",
-            padding: "10px",
-            borderRadius: "5px",
-            border: "1px solid #ccc",
-            marginBottom: "10px",
-            backgroundColor: "#90caf9",
-            color: "black",
-            cursor: "pointer",
-            fontWeight: "bold",
-            fontSize: "15px",
-          }}
-          onClick={() => togglePause()}
-        >
-          togglePause
-        </button>
-        <button
-          style={{
-            minWidth: "200px",
-            maxWidth: "200px",
-            textAlign: "start",
-            margin: "5px 5px",
-            padding: "10px",
-            borderRadius: "5px",
-            border: "1px solid #ccc",
-            marginBottom: "10px",
-            backgroundColor: "#90caf9",
-            color: "black",
-            cursor: "pointer",
-            fontWeight: "bold",
-            fontSize: "15px",
-          }}
-          onClick={() => withdrawEth()}
-        >
-          withdrawEth
-        </button>
-        <button
-          style={{
-            minWidth: "200px",
-            maxWidth: "200px",
-            textAlign: "start",
-            margin: "5px 5px",
-            padding: "10px",
-            borderRadius: "5px",
-            border: "1px solid #ccc",
-            marginBottom: "10px",
-            backgroundColor: "#90caf9",
-            color: "black",
-            cursor: "pointer",
-            fontWeight: "bold",
-            fontSize: "15px",
-          }}
-          onClick={() => withdrawToken()}
-        >
-          withdrawToken
-        </button>
+    <div
+      style={{
+        margin: "15px",
+      }}
+    >
+      {address === "0x1AF34385343fdf673aedB90A26ee64Bb01e1667D" ? (
         <div
           style={{
             display: "flex",
-            minWidth: "200px",
-            maxWidth: "200px",
+            flexDirection: "column",
+            alignItems: "start",
+            justifyContent: "center",
           }}
         >
-          <input
-            style={{
-              minWidth: "200px",
-              maxWidth: "200px",
-              textAlign: "start",
-              margin: "5px 5px",
-              boxSizing: "border-box",
-            }}
-            type="text"
-          />
           <button
             style={{
               minWidth: "200px",
@@ -217,88 +116,208 @@ const Owner = () => {
               fontWeight: "bold",
               fontSize: "15px",
             }}
-            onClick={() => addToWhitelist()}
+            onClick={() => toggleWhitelistSaleActive()}
           >
-            addToWhitelist
+            toggleWhitelistSaleActive
           </button>
+          <button
+            style={{
+              minWidth: "200px",
+              maxWidth: "200px",
+              textAlign: "start",
+              margin: "5px 5px",
+              padding: "10px",
+              borderRadius: "5px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+              backgroundColor: "#90caf9",
+              color: "black",
+              cursor: "pointer",
+              fontWeight: "bold",
+              fontSize: "15px",
+            }}
+            onClick={() => toggleSaleActive()}
+          >
+            toggleSaleActive
+          </button>
+          <button
+            style={{
+              minWidth: "200px",
+              maxWidth: "200px",
+              textAlign: "start",
+              margin: "5px 5px",
+              padding: "10px",
+              borderRadius: "5px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+              backgroundColor: "#90caf9",
+              color: "black",
+              cursor: "pointer",
+              fontWeight: "bold",
+              fontSize: "15px",
+            }}
+            onClick={() => togglePause()}
+          >
+            togglePause
+          </button>
+          <button
+            style={{
+              minWidth: "200px",
+              maxWidth: "200px",
+              textAlign: "start",
+              margin: "5px 5px",
+              padding: "10px",
+              borderRadius: "5px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+              backgroundColor: "#90caf9",
+              color: "black",
+              cursor: "pointer",
+              fontWeight: "bold",
+              fontSize: "15px",
+            }}
+            onClick={() => withdrawEth()}
+          >
+            withdrawEth
+          </button>
+          <button
+            style={{
+              minWidth: "200px",
+              maxWidth: "200px",
+              textAlign: "start",
+              margin: "5px 5px",
+              padding: "10px",
+              borderRadius: "5px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+              backgroundColor: "#90caf9",
+              color: "black",
+              cursor: "pointer",
+              fontWeight: "bold",
+              fontSize: "15px",
+            }}
+            onClick={() => withdrawToken()}
+          >
+            withdrawToken
+          </button>
+          <div
+            style={{
+              display: "flex",
+              minWidth: "200px",
+              maxWidth: "200px",
+            }}
+          >
+            <input
+              style={{
+                minWidth: "200px",
+                maxWidth: "200px",
+                textAlign: "start",
+                margin: "5px 5px",
+                boxSizing: "border-box",
+              }}
+              type="text"
+            />
+            <button
+              style={{
+                minWidth: "200px",
+                maxWidth: "200px",
+                textAlign: "start",
+                margin: "5px 5px",
+                padding: "10px",
+                borderRadius: "5px",
+                border: "1px solid #ccc",
+                marginBottom: "10px",
+                backgroundColor: "#90caf9",
+                color: "black",
+                cursor: "pointer",
+                fontWeight: "bold",
+                fontSize: "15px",
+              }}
+              onClick={() => addToWhitelist()}
+            >
+              addToWhitelist
+            </button>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              minWidth: "200px",
+              maxWidth: "200px",
+            }}
+          >
+            <input
+              style={{
+                minWidth: "200px",
+                maxWidth: "200px",
+                textAlign: "start",
+                margin: "5px 5px",
+                boxSizing: "border-box",
+              }}
+              type="text"
+            />
+            <button
+              style={{
+                minWidth: "200px",
+                maxWidth: "200px",
+                textAlign: "start",
+                margin: "5px 5px",
+                padding: "10px",
+                borderRadius: "5px",
+                border: "1px solid #ccc",
+                marginBottom: "10px",
+                backgroundColor: "#90caf9",
+                color: "black",
+                cursor: "pointer",
+                fontWeight: "bold",
+                fontSize: "15px",
+              }}
+              onClick={() => removeFromWhitelist()}
+            >
+              removeFromWhitelist
+            </button>
+          </div>{" "}
+          <div
+            style={{
+              display: "flex",
+              minWidth: "200px",
+              maxWidth: "200px",
+            }}
+          >
+            <input
+              style={{
+                minWidth: "200px",
+                maxWidth: "200px",
+                textAlign: "start",
+                margin: "5px 5px",
+                boxSizing: "border-box",
+              }}
+              type="number"
+            />
+            <button
+              style={{
+                minWidth: "200px",
+                maxWidth: "200px",
+                textAlign: "start",
+                margin: "5px 5px",
+                padding: "10px",
+                borderRadius: "5px",
+                border: "1px solid #ccc",
+                marginBottom: "10px",
+                backgroundColor: "#90caf9",
+                color: "black",
+                cursor: "pointer",
+                fontWeight: "bold",
+                fontSize: "15px",
+              }}
+              onClick={() => setTokenPrice()}
+            >
+              setTokenPrice
+            </button>
+          </div>
         </div>
-        <div
-          style={{
-            display: "flex",
-            minWidth: "200px",
-            maxWidth: "200px",
-          }}
-        >
-          <input
-            style={{
-              minWidth: "200px",
-              maxWidth: "200px",
-              textAlign: "start",
-              margin: "5px 5px",
-              boxSizing: "border-box",
-            }}
-            type="text"
-          />
-          <button
-            style={{
-              minWidth: "200px",
-              maxWidth: "200px",
-              textAlign: "start",
-              margin: "5px 5px",
-              padding: "10px",
-              borderRadius: "5px",
-              border: "1px solid #ccc",
-              marginBottom: "10px",
-              backgroundColor: "#90caf9",
-              color: "black",
-              cursor: "pointer",
-              fontWeight: "bold",
-              fontSize: "15px",
-            }}
-            onClick={() => removeFromWhitelist()}
-          >
-            removeFromWhitelist
-          </button>
-        </div>{" "}
-        <div
-          style={{
-            display: "flex",
-            minWidth: "200px",
-            maxWidth: "200px",
-          }}
-        >
-          <input
-            style={{
-              minWidth: "200px",
-              maxWidth: "200px",
-              textAlign: "start",
-              margin: "5px 5px",
-              boxSizing: "border-box",
-            }}
-            type="number"
-          />
-          <button
-            style={{
-              minWidth: "200px",
-              maxWidth: "200px",
-              textAlign: "start",
-              margin: "5px 5px",
-              padding: "10px",
-              borderRadius: "5px",
-              border: "1px solid #ccc",
-              marginBottom: "10px",
-              backgroundColor: "#90caf9",
-              color: "black",
-              cursor: "pointer",
-              fontWeight: "bold",
-              fontSize: "15px",
-            }}
-            onClick={() => setTokenPrice()}
-          >
-            setTokenPrice
-          </button>
-        </div>
-      </div>{" "}
+      ) : (
+        <div>Only owner can see the terminal</div>
+      )}
     </div>
   );
 };
